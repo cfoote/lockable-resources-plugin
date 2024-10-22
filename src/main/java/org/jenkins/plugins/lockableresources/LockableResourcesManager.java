a247b29faebac3409bec63a4aeb9f715bff96dd8
Chris Foote <cafoote@yahoo.com>
2024-10-23 03:02:16 +1300

    Reinstate the released lock messages for freestyle builds (removed by #673)

--- src/main/java/org/jenkins/plugins/lockableresources/LockableResourcesManager.java
+++ src/main/java/org/jenkins/plugins/lockableresources/LockableResourcesManager.java
@@ -649,32 +649,34 @@ public class LockableResourcesManager extends GlobalConfiguration {
         removeResources(toBeRemoved);
     }
 
-    public void unlockBuild(@Nullable Run<?, ?> build) {
+    @NonNull
+    public List<String> unlockBuild(@Nullable Run<?, ?> build) {
 
         if (build == null) {
-            return;
+            return Collections.emptyList();
         }
 
         List<String> resourcesInUse =
                 LockedResourcesBuildAction.findAndInitAction(build).getCurrentUsedResourceNames();
-
-        if (resourcesInUse.size() == 0) {
-            return;
-        }
-        unlockNames(resourcesInUse, build);
+        return unlockNames(resourcesInUse, build);
     }
 
     // ---------------------------------------------------------------------------
+    @NonNull
     @SuppressFBWarnings(value = "REC_CATCH_EXCEPTION", justification = "not sure which exceptions might be catch.")
-    public void unlockNames(@Nullable List<String> resourceNamesToUnLock, Run<?, ?> build) {
+    public List<String> unlockNames(@Nullable List<String> resourceNamesToUnLock, Run<?, ?> build) {
 
         // make sure there is a list of resource names to unlock
         if (resourceNamesToUnLock == null || resourceNamesToUnLock.isEmpty()) {
-            return;
+            return Collections.emptyList();
         }
+
+        List<String> unlockedResourceNames;
         synchronized (this.syncResources) {
+            unlockedResourceNames = new ArrayList<>(resourceNamesToUnLock);
             unlockResources(this.fromNames(resourceNamesToUnLock), build);
         }
+        return unlockedResourceNames;
     }
 
     // ---------------------------------------------------------------------------
